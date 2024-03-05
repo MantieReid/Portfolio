4fd559332fdf49afa902aaae174b6db85c75288a
Mantie <mantiereidii@gmail.com>
2024-03-05 00:12:09 -0500

    Added Vercel analytics to the root layer
    Added Vercel analytics to the root layer.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
